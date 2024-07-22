fbbab7da4a5784bab4410d0770131fe9fdd71eaf
Siddharth Kharia <siddharthkharia@gmail.com>
2024-07-23 04:52:23 +0530

    modified:   cypress.config.js 	modified:   cypress/tsconfig.json 	modified:   package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,23 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+
+  projectId: "ocnw76",
+  video:true,
+
+
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
       
   },
+  },
+  env:{
+    AMAZON_URL:"https://amazon.in/"
   },
   numTestsKeptInMemory :5,
   waitForAnimations : true
